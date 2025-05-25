eaa8eb60369d0944b238f6b04a3f541c3eeb6342
mtpauly <peregrine.pauly@gmail.com>
2025-05-25 11:52:27 -0700

    Add repeat and caps word keys

---
+++ keyboards/zsa/voyager/keymaps/mtpauly/config.h
@@ -0,0 +1,3 @@
+#define TAPPING_TERM 250
+#define CHORDAL_HOLD
+#define PERMISSIVE_HOLD
--- keyboards/zsa/voyager/keymaps/mtpauly/keymap.c
+++ keyboards/zsa/voyager/keymaps/mtpauly/keymap.c
@@ -16,9 +16,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         _______,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           _______,
         _______,        KC_V,           KC_M,           KC_L,           KC_C,           KC_P,                           KC_B,           _______,        KC_O,           KC_U,           KC_J,           _______,
-        KC_TAB,         MT(MOD_LCTL, KC_S), LT(1, KC_T), MT(MOD_LALT, KC_R), MT(MOD_LSFT, KC_D), MT(MOD_LGUI, KC_Y),    MT(MOD_RGUI, KC_F), MT(MOD_RSFT, KC_N), MT(MOD_RALT, KC_A), LT(1, KC_E), MT(MOD_RCTL, KC_I), KC_QUOTE,
-        _______,        KC_W,           KC_K,           KC_X,           KC_G,           KC_Q,                           KC_Z,           KC_H,           KC_COMM,        KC_DOT,         KC_SCLN,        _______,
-                                                                        KC_ENTER,       KC_ESCAPE,                      KC_BSPC,        KC_SPACE
+        KC_TAB,         LCTL_T(KC_S),   LT(1, KC_T),    LALT_T(KC_R),   LSFT_T(KC_D),   LGUI_T(KC_Y),                   RGUI_T(KC_F),   RSFT_T(KC_N),   RALT_T(KC_A),   LT(1, KC_E),    RCTL_T(KC_I),   KC_QUOTE,
+        CW_TOGG,        KC_W,           KC_K,           KC_X,           KC_G,           KC_Q,                           KC_Z,           KC_H,           KC_COMM,        KC_DOT,         KC_SCLN,        KC_ENTER,
+                                                                        QK_REPEAT_KEY,  KC_ESCAPE,                      KC_BSPC,        KC_SPACE
     ),
     [1] = LAYOUT(
         _______,        _______,        _______,        _______,        _______,        _______,                        _______,        _______,        _______,        _______,        _______,        _______,
